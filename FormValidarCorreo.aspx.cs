835bc92a030a2bb4a110d3b9407d44f4aa99d88f
JafetEn28 <jafetperez78@gmail.com>
2024-11-26 19:38:56 -0600

    Prueba

--- FormValidarCorreo.aspx.cs
+++ FormValidarCorreo.aspx.cs
@@ -13,5 +13,10 @@ namespace Proyecto_Analisis2
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            print("Hola mundo");
+        }
     }
 }
